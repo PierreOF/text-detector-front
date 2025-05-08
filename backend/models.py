69a5723e917729888ccba7e68d76baeb288bceab
PierreOF <marcos.pierre@upe.br>
2025-05-08 16:20:46 -0300

    remove arquivos ignorados do repositorio

--- backend/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class AnaliseResposta(BaseModel):
-    imagem_processada_base64: str
-    areas_texto: int
-    densidade_texto: float
-    confianca: int
-    area_total_px2: int
